9305b4d2c76a1a0073fdf57d35b3feef7062f49d
Average Geek <contact@theaveragegeek.com>
2009-04-22 15:38:20 +0800

    Dropping pageant.bat
    Signed-off-by: Average Geek <contact@theaveragegeek.com>

--- pageant.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start C:\Programs\Putty\pageant.exe "C:\Balaji's\My Web Sites\theaveragegeek.com\privatekey-balajivps.ppk" "C:\Balaji's\My Web Sites\theaveragegeek.com\torbox\sshprivate.ppk" "C:\Balaji's\My Web Sites\theaveragegeek.com\rcsbox\rcs-averagegeek.ppk"
-exit
